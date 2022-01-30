11abcb06762affdf01c8dbff1ff05f33c22e99bd
iynewz <81401074+iynewz@users.noreply.github.com>
2022-01-30 17:58:47 +0800

    add highlight to text

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 var myDate = new Date();
 console.log(myDate);
 
+/*characters to be escaped [.*+?^${}()|[\]\\]*/
 
-	
+function highlightSelected() {
+    let textToHighlight = window.getSelection();
+    console.log("textToHighlight", textToHighlight);
+    stringToHighlight = textToHighlight.toString().replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    let pattern = new RegExp(`${stringToHighlight}`, "gi");
+    console.log(pattern);
+    let anchorPara = textToHighlight.anchorNode.parentElement;
+    console.log("anchorPara", anchorPara);
+    console.log(anchorPara.innerHTML);
+    anchorPara.innerHTML = anchorPara.innerText.replace(pattern, match => `<mark>${match}</mark>`);
+    console.log(anchorPara.innerHTML);
+}
 
+window.addEventListener('mouseup', e => {
+    highlightSelected();
+});
